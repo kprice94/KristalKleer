e2ca51b1d549c68651f324f65167885f9a811424
SAGE\kprice <kprice@sagepath.com>
2018-02-15 18:06:35 -0500

    added toggle for open and cloing of menu

--- javascript/KristalKleer.js
+++ javascript/KristalKleer.js
@@ -2,3 +2,16 @@
 $('div.columnnav').mouseover(function() {
     $(this).addClass("overlay");
 });*/
+
+/* ---Toggle Nav Menu --*/
+
+/*open*/
+$('.burger-nav').click(function() {
+    $('nav').toggleClass('hide');
+});
+
+
+/*close*/
+$('.closebutton').click(function() {
+    $('nav').toggleClass('hide');
+});
