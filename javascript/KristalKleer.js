0f59320840a0f11b898c4a1387c844f329da2d7e
PC\Keylin <kprice94@gmail.com>
2018-02-16 00:20:35 -0500

    updated revisions

--- javascript/KristalKleer.js
+++ javascript/KristalKleer.js
@@ -5,7 +5,7 @@ $('div.columnnav').mouseover(function() {
 
 /* ---Toggle Nav Menu --*/
 
-/*open*/
+//open
 $('.burger-nav').click(function() {
     $('nav').toggleClass('hide');
     $('.burger-nav').toggleClass('hide');
@@ -13,10 +13,68 @@ $('.burger-nav').click(function() {
 });
 
 
-/*close*/
+//close
 $('.closebutton').click(function() {
     $('nav').toggleClass('hide');
         $('.burger-nav').toggleClass('hide');
           $('#logo h1').toggleClass('no-pm');
 
 });
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+
+/*scroll to top button*/
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("button").style.display = "block";
+    } else {
+        document.getElementById("button").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
